1be2912af5718f78331f4715a32682070d65a7ee
debifogel <d0548431932@gmail.com>
2025-02-09 23:45:58 +0200

    add finish node

--- node/Server.js
+++
@@ -1,7 +0,0 @@
-import renderApi from '@api/render-api';
-
-renderApi.auth('rnd_inwbYa4TRKXbbyBJKMYG2BFH2ylY');
-renderApi.listServices({includePreviews: 'true', limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
-  
